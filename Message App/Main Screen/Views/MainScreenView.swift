1dda3b0b1bf937285b000c9b75cb59d93b35238f
Joe <48731689+joecirillo@users.noreply.github.com>
2023-11-08 18:56:07 -0500

    First commit

---
+++ Message App/Main Screen/Views/MainScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  MainScreenView.swift
+//  Message App
+//
+//  Created by Joe Cirillo on 11/8/23.
+//
+
+import UIKit
+
+class MainScreenView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
