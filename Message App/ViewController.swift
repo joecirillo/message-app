9fe04cbe4f7ca4791ef752e384cb47522fe133f9
Joe <48731689+joecirillo@users.noreply.github.com>
2023-11-08 18:42:02 -0500

    Initial Commit

---
+++ Message App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Message App
+//
+//  Created by Joe Cirillo on 11/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
