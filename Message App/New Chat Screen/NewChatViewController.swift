54c3739e569cd88bfa5d84b61b89014a27e3efbf
Joe <48731689+joecirillo@users.noreply.github.com>
2023-11-14 17:57:12 -0500

    Added a function to  RegisterFirebaseManager to add a user document when a user is successfully created

--- Message App/New Chat Screen/NewChatViewController.swift
+++ Message App/New Chat Screen/NewChatViewController.swift
@@ -29,21 +29,51 @@ class NewChatViewController: UIViewController {
     
     //MARK: on add button tapped....
     @objc func onAddButtonTapped(){
-        let email = newChatScreen.textFieldEmail.text
-        
-        if email == "" {
-            //alert..
-        }else{
-            //MARK: need to get user from given email and set that as userChatting in new Chat
-            //let friend = getFriendFromFirebase()
-            //let chat = Chat(messages: [Message](), userChatting: friend)
-            //saveChatToFireStore(chat: chat)
+        if let email = newChatScreen.textFieldEmail.text{
+            
+            if email == "" {
+                //alert..
+                showUserNotEnteredAlert()
+            }
+            else{
+                // checks the users collection in Firestore to determine whether the email is a valid user
+                checkDocumentExists(email: email)
+                
+                //MARK: need to get user from given email and set that as userChatting in new Chat
+                //let friend = getFriendFromFirebase()
+                //let chat = Chat(messages: [Message](), userChatting: friend)
+                //saveChatToFireStore(chat: chat)
+            }
         }
         
     }
     
+    func checkDocumentExists(email documentID: String) {
+        let db = Firestore.firestore()
+        let documentRef = db.collection("users").document(documentID)
+
+        documentRef.getDocument { (documentSnapshot, error) in
+            if let error = error {
+                print("Error getting document: \(error.localizedDescription)")
+                return
+            }
+
+            if let documentSnapshot = documentSnapshot, documentSnapshot.exists {
+                print("The document with ID \(documentID) exists.")
+            } else {
+                print("The document with ID \(documentID) does not exist.")
+                self.showUserDoesNotExistAlert()
+                
+            }
+        }
+    }
+    
     //MARK: logic to add a contact to Firestore...
     func saveChatToFireStore(chat: Chat){
+        
+        // create a new chat here
+        
+        
         if let userEmail = currentUser!.email{
             let collectionChats = database
                 .collection("users")
